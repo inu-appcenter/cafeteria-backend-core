7ba28c8ea445ecb1e4198775e7870228f3725817
potados99 <potados99@gmail.com>
2021-10-06 23:43:14 +0900

    refactor: timeSlot의 시작과 끝을 각각 표기, 예약된 인원 수 속성 이름 변경

--- src/core/entities/booking/BookingOption.ts
+++ src/core/entities/booking/BookingOption.ts
@@ -20,8 +20,9 @@
 import Booking from './Booking';
 import {getNextDay} from '../../../utils/date';
 import CafeteriaDayOff from './CafeteriaDayOff';
-import {addMinutes, isFuture, isPast, isWeekend} from 'date-fns';
+import BookingTimeSlot from './BookingTimeSlot';
 import CafeteriaBookingParams from './CafeteriaBookingParams';
+import {areIntervalsOverlapping, isFuture, isPast, isWeekend} from 'date-fns';
 
 /**
  * 읽기 전용 예약 옵션 엔티티!
@@ -36,17 +37,12 @@ export default class BookingOption {
   /**
    * 예약하는 시간.
    */
-  timeSlot: Date;
+  timeSlotStart: Date;
 
   /**
    * 예약하는 시간대 다음 시간.
    */
-  nextTimeSlot: Date;
-
-  /**
-   * 이미 예약한 사람 수.
-   */
-  used: number;
+  timeSlotEnd: Date;
 
   /**
    * 전체 예약 가능 인원 수.
@@ -54,19 +50,26 @@ export default class BookingOption {
   capacity: number;
 
   /**
+   * 이미 예약한 사람 수.
+   */
+  reserved: number;
+
+  /**
    * 생성자 처럼 사용합니다.
    * 읽기전용이라 나만쓸거임 흥
    */
   private static async fromBookingParamsAndTimeSlot(
     bookingParams: CafeteriaBookingParams,
-    timeSlot: Date
+    timeSlot: BookingTimeSlot
   ): Promise<BookingOption> {
     return Object.assign(new BookingOption(), {
       cafeteriaId: bookingParams.cafeteriaId,
       timeSlot,
-      nextTimeSlot: addMinutes(timeSlot, bookingParams.intervalMinutes),
-      used: await Booking.howManyBookedForCafeteriaAtTimeSlot(bookingParams.cafeteriaId, timeSlot),
-      capacity: bookingParams.capacity,
+      used: await Booking.howManyBookedForCafeteriaAtTimeSlotStart(
+        bookingParams.cafeteriaId,
+        timeSlot.start
+      ),
+      capacity: timeSlot.capacity,
     });
   }
 
@@ -101,15 +104,15 @@ export default class BookingOption {
    * 식당 식별자와 타임슬롯으로 예약 옵션을 하나 가져옵니다.
    *
    * @param cafeteriaId 식당 식별자.
-   * @param timeSlot 예약 시간대(타임슬롯).
+   * @param timeSlotStart 예약 시간대(타임슬롯).
    */
-  static async findByCafeteriaAndTimeSlot(
+  static async findByCafeteriaAndTimeSlotStart(
     cafeteriaId: number,
-    timeSlot: Date
+    timeSlotStart: Date
   ): Promise<BookingOption | undefined> {
     const allOptions = await this.findForCafeteria(cafeteriaId);
 
-    return allOptions.find((o) => o.timeSlot.getTime() === timeSlot.getTime());
+    return allOptions.find((o) => o.timeSlotStart.getTime() === timeSlotStart.getTime());
   }
 
   /**
@@ -122,22 +125,28 @@ export default class BookingOption {
    * 설령 다음 날이 휴일이거나 하루 종일 휴업이더라도 해당 날짜를 기준으로 빈 배열만 가져옵니다.
    */
   static async getNextTimeSlotsInBusinessHour(
-    bookingParams: CafeteriaBookingParams
-  ): Promise<Date[]> {
+    bookingParams: CafeteriaBookingParams,
+    dayOffsGiven?: CafeteriaDayOff[]
+  ): Promise<BookingTimeSlot[]> {
     const now = new Date();
     const baseDate = bookingParams.isOverToday() ? getNextDay(now) : now;
 
-    const dayOffs = await CafeteriaDayOff.findForCafeteriaAtSameDay(
-      bookingParams.cafeteriaId,
-      baseDate
-    );
+    const dayOffs =
+      dayOffsGiven ??
+      (await CafeteriaDayOff.findForCafeteriaAtSameDay(bookingParams.cafeteriaId, baseDate));
 
-    const isNotWeekend = (slot: Date) => !isWeekend(slot);
-    const isNotOffTime = (slot: Date) => dayOffs.find((off) => off.isInOffTime(slot)) == null;
-    const isNotOver = (slot: Date) => isFuture(addMinutes(slot, bookingParams.intervalMinutes));
+    const isNotWeekend = (slot: BookingTimeSlot) => !isWeekend(slot.start);
+    const isNotOffTime = (slot: BookingTimeSlot) =>
+      dayOffs.find((off) =>
+        areIntervalsOverlapping(
+          {start: slot.start, end: slot.end},
+          {start: off.startsAt, end: off.endsAt}
+        )
+      ) == null;
+    const isNotOver = (slot: BookingTimeSlot) => isFuture(slot.end);
 
     return bookingParams
-      .allTimeSlots(baseDate)
+      .getAllTimeSlots(baseDate)
       .filter(isNotWeekend)
       .filter(isNotOffTime)
       .filter(isNotOver);
@@ -147,7 +156,7 @@ export default class BookingOption {
    * 해당 예약이 꽉 찼는가?
    */
   isFull() {
-    return this.used >= this.capacity;
+    return this.reserved >= this.capacity;
   }
 
   /**
@@ -155,7 +164,7 @@ export default class BookingOption {
    * 즉, 예약하기엔 늦은 시간대인가?
    */
   isPast() {
-    return isPast(this.nextTimeSlot);
+    return isPast(this.timeSlotEnd);
   }
 
   /**
